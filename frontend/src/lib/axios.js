32737ab4add4e5a5fd775d5a0be73bc307a44aa8
rii98 <136151503+rii98@users.noreply.github.com>
2025-10-26 18:44:16 +0545

    auth api socket fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios"
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:5173/api"
