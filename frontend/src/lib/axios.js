4d9aa3937e8ad26759b4fd8716e83092f5f4c98b
rii98 <136151503+rii98@users.noreply.github.com>
2025-10-26 16:54:29 +0545

    baseurl change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5173/api"
+    : "https://chat-app-e2ee.onrender.com/api",
   withCredentials: true,
 });
